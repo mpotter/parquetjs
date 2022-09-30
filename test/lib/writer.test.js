402992be0b08f2ba14cd699443a189da17f68910
mehtaishita <ishitamehta33@outlook.com>
2022-09-30 18:36:58 -0500

    initial setup

---
+++ test/lib/writer.test.js
@@ -0,0 +1,13 @@
+import { ParquetWriter, ParquetEnvelopeWriter, ParquetSchema } from '../../lib/writer';
+
+describe("bufferReader", () => {
+    let writer;
+
+    describe("parquetWriterHelpler", () => {
+        it("infers parquet schema correctly", () => {
+
+        })
+
+        it("")
+    })
+})
